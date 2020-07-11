af6d6f31b8d0feb6f468716fdbccb2e1a7889f1b
CHI-CHON KONG <daphanyyan@hotmail.com>
2020-07-11 15:50:24 -0700

    deleted some practice file. Pypoll challenge is the final version of code.

--- challenge.py
+++
@@ -1,8 +0,0 @@
-import csv
-import os
-#Assign variable for the file to load and the path
-file_to_load = os.path.join("resources", "election_results.csv")
-#Open the election results and read the file
-with open(file_to_load) as election_data:
-    print(election_data)
-    
